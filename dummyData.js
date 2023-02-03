a24877f12155ca26e719c8ba6587c1cc69c1979d
Leon Chung <leon.karho.chung@gmail.com>
2023-02-03 14:28:56 -0500

    hero banner styling

--- dummyData.js
+++ dummyData.js
@@ -4,20 +4,20 @@ export const menuArray = [
 		ingredients: ["pepperoni", "mushrom", "mozarella"],
 		id: 0,
 		price: 14,
-		// emoji: "🍕",
+		emoji: "🍕",
 	},
 	{
 		name: "Hamburger",
 		ingredients: ["beef", "cheese", "lettuce"],
 		price: 12,
-		// emoji: "🍔",
+		emoji: "🍔",
 		id: 1,
 	},
 	{
 		name: "Beer",
 		ingredients: ["grain, hops, yeast, water"],
 		price: 12,
-		// emoji: "🍺",
+		emoji: "🍺",
 		id: 2,
 	},
 ];
