27275bc7f6818f0cf23549957313ef7850ad97e7
Leon Chung <leon.karho.chung@gmail.com>
2023-02-03 16:12:31 -0500

    render menu items - add item to cart handler

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
 import { menuArray } from "./dummyData.js";
 
-console.log(menuArray);
+// add click event on food item buttons
+document.addEventListener("click", (event) => {
+	if (event.target.dataset.itemId) {
+		handleAddItemToCart(event.target.dataset.itemId);
+	}
+});
+
+const cartItems = [];
+
+function handleAddItemToCart(itemId) {
+	menuArray.forEach((item) => {
+		if (item.id === parseInt(itemId)) {
+			cartItems.push(item);
+		}
+	});
+
+	renderOrder();
+}
+
+function renderOrder() {
+	console.log(cartItems);
+}
+
+let menuItemHTML = "";
+menuArray.forEach((item) => {
+	let ingredients = item.ingredients.join(", ");
+	menuItemHTML += `
+                    <div class="menu-item">
+                        <p role="img" aria-details='${item.name}' class="menu-item-emoji">${item.emoji}</p>
+                        <div class="menu-item-description">
+                          <h3>${item.name}</h3>
+                          <p>${ingredients}</p>
+                          <p>$${item.price}</p>
+                        </div>
+                        <button data-item-id='${item.id}' class="add-btn">+</button>
+                    </div>
+  `;
+});
+
+function render() {
+	document.getElementById("menu-item-container").innerHTML = menuItemHTML;
+}
+
+render();
